88a2a7b35f7b4c4b7adc95968464ef3fa1f7cdb8
Gabriel Melo <melogoncalvesbiel@gmail.com>
2022-10-18 15:29:54 -0300

    feat: cria o token de autenticacao com login do usuario

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,13 +1,13 @@
 import { Request, Response } from "express";
 import { route } from "..";
 import { AuthMiddleware } from "../middlewares/auth";
-import { signIn, SignInResponse } from "../services/auth";
+import { logIn, logInResponse } from "../services/auth";
 
 export function Auth() {
   AuthMiddleware();
 
   route.get("/auth", async (req: Request, res: Response) => {
-    const response: SignInResponse = await signIn(req.body);
+    const response: logInResponse = await logIn(req.body);
 
     if (response.status) {
       return res.status(200).send(response);
--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -1,24 +1,38 @@
 import { comparePassword } from "./bcrypt";
 import { getUser, UserProps } from "../user/user";
+import jwt from "jsonwebtoken";
+import { config } from "../../config/auth";
 
-export interface SignInResponse {
+export interface logInResponse {
   message: string;
   status: boolean;
+  token?: string;
 }
 
-export async function signIn(user: UserProps) {
+export async function logIn(user: UserProps) {
   const dataUser = (await getUser(user.email)) as UserProps;
+
+  if (!dataUser) {
+    const response: logInResponse = {
+      message: "User not found",
+      status: false,
+    };
+    return response;
+  }
 
   const isAuth = await comparePassword(user.password, dataUser.password);
 
   if (isAuth) {
-    const response: SignInResponse = {
+    const response: logInResponse = {
       message: "User authenticate successfully",
       status: isAuth,
+      token: jwt.sign({ id: dataUser.id }, String(config.secret), {
+        expiresIn: config.expireIn,
+      }),
     };
     return response;
   } else {
-    const response: SignInResponse = {
+    const response: logInResponse = {
       message: "Incorrect email or password",
       status: isAuth,
     };
